4b7622ec667263df87a071b3085a431bd831bbba
SIKV <serhii.here@gmail.com>
2024-10-18 22:59:04 +0300

    Setup iOS todos view

--- iosApp/iosApp/UI/Feature/Todos/TodosView.swift
+++ iosApp/iosApp/UI/Feature/Todos/TodosView.swift
@@ -1,8 +1,36 @@
 import SwiftUI
+import Shared
 
-struct TodosView: View {
-    var body: some View {
-        Text("Todos")
+struct TodosView: ConnectedView {
+    
+    struct Props {
+        let state: TodoListState
+        
+        let onFetchAll: () -> Void
+    }
+    
+    func map(state: AppState, dispatch: @escaping DispatchFunction) -> Props {
+        return Props(
+            state: state.todoListState,
+            onFetchAll: {
+                dispatch(TodoListAction.FetchAll())
+            }
+        )
+    }
+    
+    func body(props: Props) -> some View {
+        VStack {
+            if (props.state.isLoading) {
+                ProgressView()
+            } else {
+                List(props.state.todos, id: \.self) { todo in
+                    Text(todo.title)
+                }
+            }
+        }
+        .onAppear {
+            props.onFetchAll()
+        }
     }
 }
 
